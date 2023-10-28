381f785ac8cd069158c4436c710e4e813b9ae752
Aleksei Leontev <aleksei_leontev94@mail.ru>
2023-10-28 21:13:07 +0500

    dzJavaMaven

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,68 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class BonusServiceTest {
+
+    @Test
+    void testRegistered() {
+        BonusService service = new BonusService();
+
+
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+
+        long actual = service.calculate(amount, registered);
+
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testForRegisteredLimit() {
+        BonusService service = new BonusService();
+
+
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+
+
+        long actual = service.calculate(amount, registered);
+
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testUnegisteredLimit() {
+        BonusService service = new BonusService();
+
+
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500;
+
+
+        long actual = service.calculate(amount, registered);
+
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void testUnegisteredL() {
+        BonusService service = new BonusService();
+
+
+        long amount = 2000;
+        boolean registered = false;
+        long expected = 20;
+
+
+        long actual = service.calculate(amount, registered);
+
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
